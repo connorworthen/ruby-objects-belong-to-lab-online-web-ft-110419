9e7c00fbcc606bc5877c9c5737c8ca27b6c3b443
connor worthen <connorw220@protonmail.com>
2019-11-17 01:12:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,20 +1,16 @@
 class Post
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :name
   def initiative(title)
     @title = title
   end
 
   def author(post)
     @post = post
-    @post = Post.new(params[:post])
-    @post.username = current_user.username
-    @post.user_id = current_user.id
-    if @post.save
-      redirect_to action: "index"
-      @posts = Post.find(:all)
-    else
-      render action: 'new'
-    end
   end
+
+  def name
+    @name = name
+  end
+    
 
 end
