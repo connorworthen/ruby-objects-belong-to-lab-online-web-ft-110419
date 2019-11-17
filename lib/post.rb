a411e3936ba6f10d4d074198db2f5d53835eaf02
connor worthen <connorw220@protonmail.com>
2019-11-17 01:08:47 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,17 @@
+class Post
+  attr_accessor :title, :author
+  def initiative(title)
+    @title = title
+  end
+
+  def author(post)
+    @post = post
+    @post = Post.new(params[:post])
+@post.username = current_user.username
+@post.user_id = current_user.id
+if @post.save
+  redirect_to action: "index"
+  @posts = Post.find(:all)
+else
+  render action: 'new'
+end
