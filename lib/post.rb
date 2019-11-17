c5bfaf54406e7ff2cdd40e282dc40a760a5bdc1e
connor worthen <connorw220@protonmail.com>
2019-11-17 01:09:53 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -7,11 +7,14 @@ class Post
   def author(post)
     @post = post
     @post = Post.new(params[:post])
-@post.username = current_user.username
-@post.user_id = current_user.id
-if @post.save
-  redirect_to action: "index"
-  @posts = Post.find(:all)
-else
-  render action: 'new'
+    @post.username = current_user.username
+    @post.user_id = current_user.id
+      if @post.save
+        redirect_to action: "index"
+        @posts = Post.find(:all)
+      else
+        render action: 'new'
+      end
+    end
+  end
 end
